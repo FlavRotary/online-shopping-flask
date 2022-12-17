52b6859119f768cdbd783a495282cdc901a0aba9
Teodor Mircea Piron <teodormircea95@gmail.com>
2022-12-17 14:55:04 +0200

    Reorganized Mongo - Flask-RESTful/Api interraction Updated Docker file Updated requirements.txt

--- src/user/user.py
+++ src/user/user.py
@@ -1,26 +1,21 @@
-from flask import Flask, jsonify
-import pymongo
-from pymongo import MongoClient
-from auth import auth
-from views import views
+from flask import Flask, jsonify, current_app, g
+from flask_restful import Api
+from flask_pymongo import MongoClient
+
+from auth import Login
 
 app = Flask(__name__)
 
-def get_db():
-    client = MongoClient(host='test_mongodb',
-                         port=27017, 
-                         username='root', 
-                         password='pass',
-                        authSource="admin")
-    db = client["users"]
-    return db
+api = Api(app)
+
+api.add_resource(Login, '/api/auth')
 
 #@app.route("/")
 #def hello_world():
 #    return "<p>Hello from user!</p>"
 
-app.register_blueprint(views, url_prefix='/')
-app.register_blueprint(auth, url_prefix='/auth')
+#app.register_blueprint(views, url_prefix='/')
+#app.register_blueprint(auth, url_prefix='/auth')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
