a57de63169d785e688f1a6f4066b12400d701c4c
Teodor Mircea Piron <teodormircea95@gmail.com>
2022-12-28 21:34:04 +0200

    Left over files from previous commit

--- src/user/user.py
+++ src/user/user.py
@@ -2,20 +2,17 @@ from flask import Flask, jsonify, current_app, g
 from flask_restful import Api
 from flask_pymongo import MongoClient
 
-from auth import Login
+from Auth import Login
+from CreateUser import UsersList
+from UserData import ReturnUser
 
 app = Flask(__name__)
 
 api = Api(app)
 
 api.add_resource(Login, '/api/auth')
-
-#@app.route("/")
-#def hello_world():
-#    return "<p>Hello from user!</p>"
-
-#app.register_blueprint(views, url_prefix='/')
-#app.register_blueprint(auth, url_prefix='/auth')
+api.add_resource(UsersList, 'api/users/')
+api.add_resource(ReturnUser, 'api/users/<string:uuid>')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
